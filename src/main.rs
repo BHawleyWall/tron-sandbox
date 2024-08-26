6d25d7795961be178e6293b192b27864e5221f0c
Bartis Hawley-Wall <bartis.hawley-wall@metaco.com>
2024-08-26 11:29:53 -0700

    clap CLI framework stubbed out

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+use std::process::ExitCode;
+
+use clap::Parser;
+
+const SHASTA_TESTNET_FULLNODE: &str = "grpc.shasta.trongrid.io:50051";
+const SHASTA_TESTNET_SOLIDITY: &str = "grpc.shasta.trongrid.io:50052";
+const SHASTA_TESTNET_JSONRPC: &str = "https://api.shasta.trongrid.io/jsonrpc";
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about)]
+struct Cli {
+    /// Fullnode endpoint
+    #[clap(short, long, default_value = SHASTA_TESTNET_FULLNODE)]
+    fullnode: String,
+
+    /// Solidity endpoint
+    #[clap(short, long, default_value = SHASTA_TESTNET_SOLIDITY)]
+    solidity: String,
+
+    /// JSON-RPC endpoint
+    #[clap(short, long, default_value = SHASTA_TESTNET_JSONRPC)]
+    jsonrpc: String,
+}
+
+fn run_cli() -> Result<(), ()> {
+    let args = Cli::parse();
+
+    println!("{:?}", args);
+
+    Ok(())
+}
+
+fn main() -> ExitCode {
+    match run_cli() {
+        Ok(_) => ExitCode::SUCCESS,
+        Err(_) => ExitCode::FAILURE,
+    }
 }
